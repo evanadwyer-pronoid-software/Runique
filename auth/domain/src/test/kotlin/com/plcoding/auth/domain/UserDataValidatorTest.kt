fa8741016011f9292ffe35a60276cae61243abea
evanadwyer <evanadwyer@pronoid-software.com>
2024-10-18 15:59:55 -0700

    Testing 4.1 set up junit5
    Signed-off-by: evanadwyer <evanadwyer@pronoid-software.com>

---
+++ auth/domain/src/test/kotlin/com/plcoding/auth/domain/UserDataValidatorTest.kt
@@ -0,0 +1,13 @@
+package com.plcoding.auth.domain
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class UserDataValidatorTest {
+
+    @Test
+    fun `test 2 and 2`() {
+        assertThat(2 + 2).isEqualTo(4)
+    }
+}
