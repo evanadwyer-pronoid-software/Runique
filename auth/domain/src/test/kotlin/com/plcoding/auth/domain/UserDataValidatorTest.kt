4172805b8f80a76a8a5f9c3943a566e0c9521334
evanadwyer <evanadwyer@pronoid-software.com>
2024-10-18 16:21:44 -0700

    Testing 4.2 simple unit test
    Signed-off-by: evanadwyer <evanadwyer@pronoid-software.com>

--- auth/domain/src/test/kotlin/com/plcoding/auth/domain/UserDataValidatorTest.kt
+++ auth/domain/src/test/kotlin/com/plcoding/auth/domain/UserDataValidatorTest.kt
@@ -2,12 +2,35 @@ package com.plcoding.auth.domain
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class UserDataValidatorTest {
 
-    @Test
-    fun `test 2 and 2`() {
-        assertThat(2 + 2).isEqualTo(4)
+    private lateinit var userDataValidator: UserDataValidator
+
+    @BeforeEach
+    fun setUp() {
+        userDataValidator = UserDataValidator(
+            patternValidator = object : PatternValidator {
+                override fun matches(value: String): Boolean {
+                    return true
+                }
+            }
+        )
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "Test12345, true",
+        "test12345, false",
+        "12345, false",
+        "Test-1234, true",
+        "TEST12345, false"
+    )
+    fun `validate password`(password: String, expectedIsValid: Boolean) {
+        val state = userDataValidator.validatePassword(password)
+        assertThat(state.isValidPassword).isEqualTo(expectedIsValid)
     }
 }
