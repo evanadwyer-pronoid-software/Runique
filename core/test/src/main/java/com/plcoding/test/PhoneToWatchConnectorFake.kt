a357196305de52f5ec22cb714539115b4d15cda7
evanadwyer <evanadwyer@pronoid-software.com>
2024-10-18 16:49:05 -0700

    Testing 4.3 sharing test utility
    Signed-off-by: evanadwyer <evanadwyer@pronoid-software.com>

---
+++ core/test/src/main/java/com/plcoding/test/PhoneToWatchConnectorFake.kt
@@ -0,0 +1,44 @@
+package com.plcoding.test
+
+import com.plcoding.core.connectivity.domain.DeviceNode
+import com.plcoding.core.connectivity.domain.messaging.MessagingAction
+import com.plcoding.core.connectivity.domain.messaging.MessagingError
+import com.plcoding.core.domain.util.EmptyResult
+import com.plcoding.core.domain.util.Result
+import com.plcoding.run.domain.WatchConnector
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class PhoneToWatchConnectorFake : WatchConnector {
+
+    var sendError: MessagingError? = null
+
+    private val _isTrackable = MutableStateFlow(true)
+    private val _connectedDevice = MutableStateFlow<DeviceNode?>(null)
+    private val _messagingActions = MutableSharedFlow<MessagingAction>()
+
+    override val connectedDevice: StateFlow<DeviceNode?>
+        get() = _connectedDevice.asStateFlow()
+    override val messagingActions: Flow<MessagingAction>
+        get() = _messagingActions.asSharedFlow()
+
+    override suspend fun sendActionToWatch(action: MessagingAction): EmptyResult<MessagingError> {
+        return if (sendError != null) {
+            Result.Error(sendError!!)
+        } else {
+            Result.Success(Unit)
+        }
+    }
+
+    override fun setIsTrackable(isTrackable: Boolean) {
+        this._isTrackable.value = isTrackable
+    }
+
+    suspend fun sendFromWatchToPhone(action: MessagingAction) {
+        _messagingActions.emit(action)
+    }
+}
