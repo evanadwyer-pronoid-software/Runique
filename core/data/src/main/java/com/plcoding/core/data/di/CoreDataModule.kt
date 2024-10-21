980650c6c29bbe33b0695f6d8b3cdb1044d7058f
evanadwyer <evanadwyer@pronoid-software.com>
2024-10-21 11:52:41 -0500

    Testing 4.5 setting up shared android test utility
    Signed-off-by: evanadwyer <evanadwyer@pronoid-software.com>

--- core/data/src/main/java/com/plcoding/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/plcoding/core/data/di/CoreDataModule.kt
@@ -1,18 +1,18 @@
 package com.plcoding.core.data.di
 
-import android.content.SharedPreferences
 import com.plcoding.core.data.auth.EncryptedSessionStorage
 import com.plcoding.core.data.networking.HttpClientFactory
 import com.plcoding.core.data.run.OfflineFirstRunRepository
 import com.plcoding.core.domain.SessionStorage
 import com.plcoding.core.domain.run.RunRepository
+import io.ktor.client.engine.cio.CIO
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
-        HttpClientFactory(get()).build()
+        HttpClientFactory(get()).build(CIO.create())
     }
     singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 
